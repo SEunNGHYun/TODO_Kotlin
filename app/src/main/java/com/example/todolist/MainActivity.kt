2d61fddf0c8ba2004f1d2531a9ada5376ef621bb
yunseunghyeon <ysh12274856@gmail.com>
2026-02-24 22:36:29 +0900

    7강 듣기

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -6,90 +6,57 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.todolist.ui.theme.TODOlistTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TODOlistTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TodoListScreen(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+    enableEdgeToEdge()
+    setContent {
+        TODOlistTheme {
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                TodoListScreen(
+                    name = "Android",
+                    modifier = Modifier.padding(innerPadding)
+                )
             }
         }
-        println("on Create")
-        Log.i("LIFECYCLE", "onCreate()")
-        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show()
-
     }
-
-    override fun onStart() {
-        super.onStart()
-        println("on Start")
-        Log.i("LIFECYCLE", "onStart()")
-        Toast.makeText(this, "onStart()", Toast.LENGTH_SHORT).show()
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        println("onResume")
-        Log.i("LIFECYCLE", "onResume()")
-        Toast.makeText(this, "onResume()", Toast.LENGTH_SHORT).show()
-
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        println("onRestart")
-        Log.i("LIFECYCLE", "onRestart()")
-        Toast.makeText(this, "onRestart()", Toast.LENGTH_SHORT).show()
-
-    }
-
-    override fun onPause() {
-        super.onPause()
-        println("onPause")
-        Log.i("LIFECYCLE", "onPause()")
-        Toast.makeText(this, "onPause()", Toast.LENGTH_SHORT).show()
-
-    }
-
-    override fun onStop() {
-        super.onStop()
-        println("onStop")
-        Log.i("LIFECYCLE", "onStop()")
-        Toast.makeText(this, "onStop()", Toast.LENGTH_SHORT).show()
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        println("onDestroy")
-        Log.i("LIFECYCLE", "onDestroy()")
-        Toast.makeText(this, "onDestroy()", Toast.LENGTH_SHORT).show()
-
     }
 }
 
 @Composable
 fun TodoListScreen(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(){
+        Text("Conver")
+        OutlinedTextField("", {
+
+        })
+        Row(){
+            val context = LocalContext.current
+            Button(
+                {Toast.
+                makeText(context,
+                    "Thank for click",
+                    Toast.LENGTH_LONG).show()
+                }){
+                Text("클릭")
+            }
+        }
+    }
+    Text("result")
 }
 
 @Preview(showBackground = true)
