4ff5b261936e279831e6beefc7f91cfe588a5fa1
mobilityn-owner <ysh12274856@gmail.com>
2026-02-25 15:10:21 +0900

    보물 찾기 게임?

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,68 +1,121 @@
 package com.example.todolist
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 import com.example.todolist.ui.theme.TODOlistTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
-    setContent {
-        TODOlistTheme {
-            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                TodoListScreen(
-                    name = "Android",
-                    modifier = Modifier.padding(innerPadding)
-                )
+        enableEdgeToEdge()
+        setContent {
+            TODOlistTheme {
+                Surface (modifier = Modifier.fillMaxSize()) {
+                    CaptainGame()
+                }
             }
         }
     }
-    }
+
+
 }
 
 @Composable
-fun TodoListScreen(name: String, modifier: Modifier = Modifier) {
-    Column(){
-        Text("Conver")
-        OutlinedTextField("", {
+fun CaptainGame () {
+    val treasuresFound = remember { mutableStateOf(0)}
+    val direction = remember { mutableStateOf("North") }
+    val stormOrTreasure = remember { mutableStateOf("") }
 
-        })
-        Row(){
-            val context = LocalContext.current
-            Button(
-                {Toast.
-                makeText(context,
-                    "Thank for click",
-                    Toast.LENGTH_LONG).show()
-                }){
-                Text("클릭")
+    Column(Modifier.fillMaxSize(), Arrangement.Center) {
+
+        Box(Modifier.fillMaxWidth() )
+
+            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+
+                Text("treasure : ${treasuresFound.value}")
+                Text("direction : ${direction.value}")
+                Text("stormOrTreasure ${stormOrTreasure.value}")
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text("향해를~", fontSize = 35.sp)
+                Spacer(modifier = Modifier.height(25.dp))
+                Button({
+                    direction.value = "North"
+                    if (Random.nextBoolean()){
+                        treasuresFound.value += 1
+                        stormOrTreasure.value = "Found a Treasure!"
+                    } else {
+                        stormOrTreasure.value = "Storm Ahead!"
+                    }
+                }) { Text("North") }
+                Row(Modifier.fillMaxWidth(), Arrangement.Center) {
+                    Button({
+                        direction.value = "West"
+                        if (Random.nextBoolean()){
+                            treasuresFound.value += 1
+                            stormOrTreasure.value = "Found a Treasure!"
+                        } else {
+                            stormOrTreasure.value = "Storm Ahead!"
+                        }
+                    }) { Text("West") }
+                    Spacer(Modifier.width(20.dp))
+                    Button({
+                        direction.value = "East"
+                        if (Random.nextBoolean()){
+                            treasuresFound.value += 1
+                            stormOrTreasure.value = "Found a Treasure!"
+                        } else {
+                            stormOrTreasure.value = "Storm Ahead!"
+                        }
+                    }) { Text("East") }
+                }
+                Button({
+                    direction.value = "South"
+                    if (Random.nextBoolean()){
+                        treasuresFound.value += 1
+                        stormOrTreasure.value = "Found a Treasure!"
+                    } else {
+                        stormOrTreasure.value = "Storm Ahead!"
+                    }
+                }) { Text("South") }
             }
         }
     }
-    Text("result")
-}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewApp() {
     TODOlistTheme {
-        TodoListScreen("Android")
+        CaptainGame()
     }
 }
