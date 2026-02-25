42e0c6e859cfaf1da3e5f16039dc34bc329711dc
mobilityn-owner <ysh12274856@gmail.com>
2026-02-25 17:19:00 +0900

    단위변환기

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.todolist
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,22 +16,34 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 import com.example.todolist.ui.theme.TODOlistTheme
+import kotlin.math.roundToInt
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +53,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TODOlistTheme {
                 Surface (modifier = Modifier.fillMaxSize()) {
-                    CaptainGame()
+                    TextInputArea()
                 }
             }
         }
@@ -50,72 +63,103 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CaptainGame () {
-    val treasuresFound = remember { mutableStateOf(0)}
-    val direction = remember { mutableStateOf("North") }
-    val stormOrTreasure = remember { mutableStateOf("") }
+fun TextInputArea () {
 
-    Column(Modifier.fillMaxSize(), Arrangement.Center) {
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
 
-        Box(Modifier.fillMaxWidth() )
+    var inputUnit by remember { mutableStateOf("cm") }
+    var outputUnit by remember { mutableStateOf("cm 센치미터") }
 
-            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+    var isExpand by remember { mutableStateOf(false) }
+    var onExpand by remember { mutableStateOf(false) }
 
-                Text("treasure : ${treasuresFound.value}")
-                Text("direction : ${direction.value}")
-                Text("stormOrTreasure ${stormOrTreasure.value}")
-                Spacer(modifier = Modifier.height(12.dp))
+    val conversion = remember { mutableStateOf(0.01) }
 
-                Text("향해를~", fontSize = 35.sp)
-                Spacer(modifier = Modifier.height(25.dp))
-                Button({
-                    direction.value = "North"
-                    if (Random.nextBoolean()){
-                        treasuresFound.value += 1
-                        stormOrTreasure.value = "Found a Treasure!"
-                    } else {
-                        stormOrTreasure.value = "Storm Ahead!"
-                    }
-                }) { Text("North") }
-                Row(Modifier.fillMaxWidth(), Arrangement.Center) {
-                    Button({
-                        direction.value = "West"
-                        if (Random.nextBoolean()){
-                            treasuresFound.value += 1
-                            stormOrTreasure.value = "Found a Treasure!"
-                        } else {
-                            stormOrTreasure.value = "Storm Ahead!"
-                        }
-                    }) { Text("West") }
-                    Spacer(Modifier.width(20.dp))
-                    Button({
-                        direction.value = "East"
-                        if (Random.nextBoolean()){
-                            treasuresFound.value += 1
-                            stormOrTreasure.value = "Found a Treasure!"
-                        } else {
-                            stormOrTreasure.value = "Storm Ahead!"
-                        }
-                    }) { Text("East") }
-                }
-                Button({
-                    direction.value = "South"
-                    if (Random.nextBoolean()){
-                        treasuresFound.value += 1
-                        stormOrTreasure.value = "Found a Treasure!"
-                    } else {
-                        stormOrTreasure.value = "Storm Ahead!"
-                    }
-                }) { Text("South") }
-            }
-        }
+    val customTextStyle = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontSize = 16.sp,
+        color = Color.Black
+    )
+    fun convertUnits() {
+        val inputValueDouble = inputValue.toDoubleOrNull() ?: 0.0;
+        val result = (inputValueDouble * conversion.value * 100.0).roundToInt() / 100.0
+        outputValue = result.toString()
     }
 
+    Column(Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text("")
+        Spacer(Modifier.height(16.dp))
+        OutlinedTextField(value = inputValue, onValueChange = {
+            inputValue = it
+        }, label = {Text("Enter!")})
+        Spacer(Modifier.height(16.dp))
+
+        Row {
+            Box {
+                Button(onClick = { isExpand = true }) {
+                    Text("Select");
+                    Icon(
+                        Icons.Outlined.ArrowDropDown,
+                        contentDescription = "unit dropdown"
+                    )
+                }
+                DropdownMenu(expanded = isExpand, {
+                    isExpand = false
+                }) {
+                    DropdownMenuItem({ Text("cm 센치미터") }, {
+                        isExpand = false
+                        inputUnit = "cm"
+                        conversion.value = 0.01
+                        convertUnits()
+                    })
+                    DropdownMenuItem({ Text("mm 밀리미터") }, {
+                        isExpand = false
+                        inputUnit = "mm"
+                        conversion.value = 0.001
+                        convertUnits()
+                    })
+                    DropdownMenuItem({ Text("m 미터") }, {
+                        isExpand = false
+                        inputUnit = "M"
+                        conversion.value = 1.0
+                        convertUnits()
+                    })
+                    DropdownMenuItem({ Text("km 칼로미터") }, {
+                        isExpand = false
+                        inputUnit = "KM"
+                        conversion.value = 100.0
+                        convertUnits()
+                    })
+                }
+            }
+            Spacer(Modifier.width(12.dp))
+            Box{
+                Button(onClick = {onExpand = true}){
+                    Text("Select")
+                    Icon(
+                        Icons.Outlined.ArrowDropDown,
+                        contentDescription = "unit dropdown")}
+                DropdownMenu(expanded = onExpand, {
+                    onExpand = false
+                }) {
+                    DropdownMenuItem({ Text("cm")}, {})
+                    DropdownMenuItem({ Text("mm")}, {})
+                    DropdownMenuItem({ Text("m")}, {})
+                    DropdownMenuItem({ Text("km")}, {})
+                }
+            }
+        }
+        Text("RESUlt : ${outputValue}", TextStyle = MaterialTheme.typography.headlineMedium)
+    }
+    }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewApp() {
     TODOlistTheme {
-        CaptainGame()
+        TextInputArea()
     }
 }
